d2fe38c0ecd59c7602b9d6a1d196992a0d12af0a
Hannia-3 <Hannia.EscPM@gmail.com>
2025-06-26 22:05:09 -0500

    Update Hero.java

--- Hero.java
+++ Hero.java
@@ -1,1 +1,31 @@
+public class Hero {
 
+  // attributes
+  private String name;
+  private int powerLevel;
+  private String affiliation;
+
+  // the constructor
+  public Hero(String name, int powerLevel, String affiliation) {
+      this.name = name;
+      this.powerLevel = powerLevel;
+      this.affiliation = affiliation;
+  }
+
+  // getter methods
+  public String getName() {
+      return name;
+  }
+  public int getPowerLevel() {
+      return powerLevel;
+  }
+  public String getAffiliation() {
+      return affiliation;
+  }
+
+  // toString method
+  @Override
+  public String toString() {
+      return "Hero: " + name + " | Power: " + powerLevel + " | Affiliation: " + affiliation;
+  }
+}
